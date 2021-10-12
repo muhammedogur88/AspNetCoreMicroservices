56890f2f1a1cc634bf25ffd44af159071b3808a9
Muhammed <muhammed.ogur@gmail.com>
2021-10-13 00:21:34 +0300

    Basket.API entities are added Repositories methods are done.

---
+++ src/Services/Basket/BasketAPI/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using BasketAPI.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace BasketAPI.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _rediscache;
+
+        public BasketRepository(IDistributedCache rediscache)
+        {
+            _rediscache = rediscache ?? throw new ArgumentNullException(nameof(rediscache));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _rediscache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _rediscache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _rediscache.RemoveAsync(userName);
+        }
+
+    }
+}
